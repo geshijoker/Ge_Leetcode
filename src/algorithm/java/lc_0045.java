2c4e49fd9d1d25574a20457f2b2c1f23b94c0412
Ge Shi <8281880+geshijoker@users.noreply.github.com>
2018-09-27 22:03:01 -0400

    Create lc_0045.java
    add 45. Jump Game II

---
+++ src/algorithm/java/lc_0045.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int jump(int[] nums) {
+        if(nums == null || nums.length < 2){
+            return 0;
+        }
+        int res = 0;
+        int curMaxArea = 0;
+        int maxNext = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            maxNext = Math.max(maxNext, nums[i] + i);
+            if(i == curMaxArea){
+                res++;
+                curMaxArea = maxNext;
+            }
+        }
+        return res;
+    }
+}
