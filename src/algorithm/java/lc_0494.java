adb50f18a8213392a36f5484efc2779e95c3829c
Ge Shi <8281880+geshijoker@users.noreply.github.com>
2019-05-05 19:42:33 -0400

    Create lc_0494.java

---
+++ src/algorithm/java/lc_0494.java
@@ -0,0 +1,24 @@
+class Solution {
+    int res = 0;
+    public int findTargetSumWays(int[] nums, int S) {
+        int[][] memo = new int[nums.length][2001];
+        for (int[] row: memo)
+            Arrays.fill(row, Integer.MIN_VALUE);
+        return helper(nums, 0, 0, S, memo);
+    }
+    public int helper(int[] nums, int index, int sum, int S, int[][] memo) {
+        if (index == nums.length) {
+            if (sum == S)
+                return 1;
+            else
+                return 0;
+        }
+        if (memo[index][sum + 1000] != Integer.MIN_VALUE) {
+            return memo[index][sum + 1000];
+        }
+        int add = helper(nums, index + 1, sum + nums[index], S, memo);
+        int subtract = helper(nums, index + 1, sum - nums[index], S, memo);
+        memo[index][sum + 1000] = add + subtract;
+        return memo[index][sum + 1000];
+    }
+}
