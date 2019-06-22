5b9ce1f800f9734d64f31305a8137ea75eda8fb6
Ge Shi <8281880+geshijoker@users.noreply.github.com>
2019-06-22 10:22:49 +0800

    Update lc_0050.java

--- src/algorithm/java/lc_0050.java
+++ src/algorithm/java/lc_0050.java
@@ -1,24 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        double r = 1;
-        if (n == Integer.MIN_VALUE) {
-            r = 1 / x;
-            n = n + 1;
-        }
-        
+        if (x == 1 || n == 0) return 1;
+        if (n == 1) return x;
         if (n < 0) {
-            n = -n;
-            x = 1 / x;
+            return 1 / (x*myPow(x, -1*(n+1)));
         }
-        
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                r = r * x;
+        double res = 1;
+        while (n > 1) {
+            if (n % 2 == 1) {
+                res *= x;
+                n -= 1;
             }
-            x = x * x;
-            n = n >> 1;
+            n = n/2;
+            x = x*x;
         }
-        
-        return r;
+
+        return res*x;
     }
 }
