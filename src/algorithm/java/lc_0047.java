a639b110c701145609a9453fd9e88e60a2952bba
Ge Shi <8281880+geshijoker@users.noreply.github.com>
2019-07-03 19:41:37 -0700

    Update lc_0047.java

--- src/algorithm/java/lc_0047.java
+++ src/algorithm/java/lc_0047.java
@@ -1,37 +1,25 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        if(nums==null || nums.length<1) return res;
         Arrays.sort(nums);
-        backtrack(res, new ArrayList<Integer>(), nums, nums.length);
-        
+        boolean[] used = new boolean[nums.length];
+        List<Integer> temp = new ArrayList<>();
+        helper(nums, res, temp, used);
         return res;
     }
-    
-    public void backtrack(List<List<Integer>> res, List<Integer> tmp, int[] nums, int len) {
-        if (len == tmp.size()) {
-            res.add(new ArrayList(tmp));
+    public void helper(int[] nums, List<List<Integer>> res, List<Integer> temp, boolean[] used) {
+        if(temp.size()>=nums.length) {
+            res.add(new ArrayList<>(temp));
             return;
         }
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (i>0 && nums[i] == nums[i-1]) continue;
-            
-            tmp.add(nums[i]);
-            backtrack(res, tmp, removedArray(nums, i), len);
-            tmp.remove(tmp.size()-1);
+        for(int i=0;i<nums.length;i++) {
+            if(used[i] || i>0 && nums[i]==nums[i-1] && used[i-1]) continue;
+            temp.add(nums[i]);
+            used[i] = true;
+            helper(nums, res, temp, used);
+            temp.remove(temp.size()-1);
+            used[i] = false;
         }
     }
-    
-    public int[] removedArray(int[] nums, int x) {
-        int[] re = new int[nums.length-1];
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i == x)
-                continue;
-            else 
-                re[j++] = nums[i];
-        }
-        
-        return re;
-    }   
 }
